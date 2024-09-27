0fddd72779dd8c8d82ce22cb97e574871ad7a4b6
jamirali <jamiraliwebdeveloper@gmail.com>
2024-09-27 10:32:41 +0600

    updated

--- src/components/Protected_Route/ProtectedRoute.tsx
+++ src/components/Protected_Route/ProtectedRoute.tsx
@@ -1,12 +1,13 @@
 import { useAppSelector } from "../../hooks/hooks";
 import { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { userToken } from "../../redux/features/authSlice";
 
 
 const ProtectedRoute = ({children}: {children: ReactNode}) => {
     const token = useAppSelector(userToken);
-    return token ? children : <Navigate to="/login" replace={true} />
+    const location = useLocation();
+    return token ? children : <Navigate to="/login" state={{from:location }} replace={true} />
 };
 
 export default ProtectedRoute;
